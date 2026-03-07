5d0e874a4cb4289025eda41c2680b343268bed39
LunarEclipse <949886@qq.com>
2026-03-07 14:10:19 +0800

    Add 2D platformer assets.

---
+++ 2D/Platformer/scripts/csharp/PlatformerCharacterController2D.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class PlatformerCharacterController2D : Node2D
+{
+    [Export] CharacterBody2D characterBody;
+    [Export] AnimationPlayer animationPlayer;
+    [Export] AnimationTree animationTree;
+    
+}
